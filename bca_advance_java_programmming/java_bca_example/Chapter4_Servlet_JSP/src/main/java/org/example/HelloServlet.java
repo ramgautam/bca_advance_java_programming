44610dcb78d04d22e432b0e00c5a8b3da6629fd2
ramgautam <ramgau@gmail.com>
2024-07-09 20:30:14 +0545

    Chapter 4 Servlet and JSP

---
+++ bca_advance_java_programmming/java_bca_example/Chapter4_Servlet_JSP/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Hello, " + name + "!</h2>");
+    }
+}
---
+++ bca_advance_java_programmming/java_bca_example/Chapter4_Servlet_JSP/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ramgautam
+  Date: 09/07/2024
+  Time: 20:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello, World!</h2>
+<form action="hello" method="post">
+    <input type="text" name="name" placeholder="Enter your name"/>
+    <input type="submit" value="Submit"/>
+</form>
+</body>
+</html>
